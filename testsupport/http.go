fad6650a4e6fe3538d87902ce11af9b36b89d699
shoubhik bose <sbose78@gmail.com>
2018-06-29 17:18:20 +0530

    refactor http

--- testsupport/http.go
+++ testsupport/http.go
@@ -28,17 +28,3 @@ func NewDummyHttpDoer() *DummyHttpDoer {
 	doer := &HttpClientDoer{HttpClient: client}
 	return &DummyHttpDoer{HttpClientDoer: doer, Client: client}
 }
-
-type HttpClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
-
-// HttpClientDoer implements HttpDoer
-type HttpClientDoer struct {
-	HttpClient HttpClient
-}
-
-// Do overrides Do method of the default goa client Doer. It's needed for mocking http clients in tests.
-func (d *HttpClientDoer) Do(ctx context.Context, req *http.Request) (*http.Response, error) {
-	return d.HttpClient.Do(req)
-}
